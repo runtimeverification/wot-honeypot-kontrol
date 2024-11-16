157a7077f23e48f8774a3090491ecaf89d63e8d0
palinatolmach <polina.tolmach@gmail.com>
2024-11-16 10:32:24 +0700

    Update the test with pranking, `vm.store` setup

--- test/WoTTest.t.sol
+++ test/WoTTest.t.sol
@@ -23,16 +23,17 @@ contract WoTTest is Test, KontrolCheats {
         
         address addr_of_interest = address(0x14E136e385c79C1D0548a96bb90E9B4e9845A73F);
         vm.store(wot_tc, bytes32(uint256(81955473079516046949633743016697847541294818689821282749996681496272635257091)), bytes32(uint256(uint160(addr_of_interest))));
-        // These fields were branching during execution in transferChecker as #lookup(?STORAGE0, slot)
-        // and ?STORAGE0 is set for 0x6de05152a0ec7d668ea54d8fb564fe4a68c07889.
-        // They also look like mapping entries so they might be related to the msg.sender which is the address of the test contract.
+        
+        // These fields are causing branching in `transferChecker` as `#lookup(?STORAGE0, slot)`
+        // and `?STORAGE0` is set for `0x6de05152a0ec7d668ea54d8fb564fe4a68c07889`.
+        // They also look like mapping entries so are related to the msg.sender which is the address of the test contract.
         // to check a slot: cast storage 0x6de05152a0ec7d668ea54d8fb564fe4a68c07889 81955473079516046949633743016697847541294818689821282749996681496272635257091 --rpc-url https://mainnet.base.org --chain 8453
 
-        // vm.store(wot_tc, bytes32(uint256(92176716289232067132282836272875158765230181307894676212186306738099599030314)), bytes32(0));
-        // vm.store(wot_tc, bytes32(uint256(114780517552515935620171773731191742103001937181785441609631115772415707507178)), bytes32(0));
-        // vm.store(wot_tc, bytes32(uint256(24890058885594772868082194210704315982890157730123342977540955694542831943486)), bytes32(0));
-        // vm.store(wot_tc, bytes32(uint256(69754373171698122749132262717936541513022023835681898290461777562612473346212)), bytes32(0));
-        // vm.store(wot_tc, bytes32(uint256(109910455024463162827942188131314426003682484546953014233182922346044597808126)), bytes32(0));
+        vm.store(wot_tc, bytes32(uint256(92176716289232067132282836272875158765230181307894676212186306738099599030314)), bytes32(0));
+        vm.store(wot_tc, bytes32(uint256(114780517552515935620171773731191742103001937181785441609631115772415707507178)), bytes32(0));
+        vm.store(wot_tc, bytes32(uint256(24890058885594772868082194210704315982890157730123342977540955694542831943486)), bytes32(0));
+        vm.store(wot_tc, bytes32(uint256(69754373171698122749132262717936541513022023835681898290461777562612473346212)), bytes32(0));
+        vm.store(wot_tc, bytes32(uint256(109910455024463162827942188131314426003682484546953014233182922346044597808126)), bytes32(0));
 
         // Setting up `transferChecker` upgraded implementation https://basescan.org/address/0xFE840992EC8793E7C4F3B93F93E56cBEf0A61612
         address wot_tc_i = address(0xFE840992EC8793E7C4F3B93F93E56cBEf0A61612);
@@ -44,6 +45,7 @@ contract WoTTest is Test, KontrolCheats {
     function test_wot_isHoneyPot(address to, uint256 value) public {
         address alice = freshAddress();
         vm.startPrank(alice);
+
         // assume no underflow during `transfer`
         bytes memory cdata = abi.encodeWithSignature("balanceOf(address)", address(this));
         (bool success, bytes memory returnData) = address(wot).call(cdata);
